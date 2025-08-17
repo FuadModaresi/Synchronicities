080b25866c04f90494dec52cb4db33118ecdf835
Fuad modaresi rad <fuadmodaresirad@gmail.com>
2025-08-17 00:45:10 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,16 +1,19 @@
 import {createLocalizedPathnamesNavigation} from 'next-intl/navigation';
- 
+
 export const locales = ['en', 'fa'] as const;
 export const localePrefix = 'always'; // Default
- 
-// The `pathnames` object holds pairs of internal
-// and external paths, separated by locale.
+
 export const pathnames = {
-  // If all locales use the same path, use the
-  // shared `pathnames` object
   '/': '/',
-  '/dashboard': '/dashboard'
+  '/dashboard': {
+    en: '/dashboard',
+    fa: '/dashboard',
+  },
 };
- 
+
 export const {Link, redirect, usePathname, useRouter, getPathname} =
-  createLocalizedPathnamesNavigation({locales, localePrefix, pathnames});
+  createLocalizedPathnamesNavigation({
+    locales,
+    localePrefix,
+    pathnames: pathnames as typeof pathnames & Record<string & {}, string>,
+  });
