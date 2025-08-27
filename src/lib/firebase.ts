8739b6cdd28418984520c25e017a82dd04303d99
Fuad modaresi rad <fuadmodaresirad@gmail.com>
2025-08-27 12:26:17 +0000

    [00:23:31.799] Running build in Washington, D.C., USA (East) – iad1 [00:

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,21 +9,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-<<<<<<< HEAD
-  authDomain: "day-weaver-q3g5q.firebaseapp.com",
-  projectId: "day-weaver-q3g5q",
-  storageBucket: "day-weaver-q3g5q.firebasestorage.app",
-  messagingSenderId: "117331109533",
-  appId: "1:117331109533:web:55bf9476d0f1304eaf5674",
-  measurementId: "G-RWFEBSWT24"
-=======
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
->>>>>>> e37a078375c3ecf0751ca4d26a4350091b58d90c
 };
 
 // Helper function to initialize Firebase
